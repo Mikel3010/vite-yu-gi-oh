91ea4bc2adb9e667ffa0a18d118548df7eb7880b
Mikel3010 <mikelgremi30@gmail.com>
2023-03-27 20:20:47 +0200

    aggiunta img

--- vite-yu-gi-ho/src/components/AppMain.vue
+++ vite-yu-gi-ho/src/components/AppMain.vue
@@ -33,7 +33,7 @@ export default{
 <div class="container_cards">
     <div class="cards" v-for="card in cards">
         <div class="card">
-            <img src="" alt="">
+            <img :src="card.card_images[0].image_url" alt="">
         </div>
         <div class="info_card">
             <h3>{{ card.name }}</h3>
@@ -52,20 +52,37 @@ export default{
 .header_card{
     color: white;
     background-color: black;
-    padding: 15px;
+    padding: 20px;
+    margin-left: 10px;
+    margin-right: 10px;
 }
 .main_container{
-    margin-left: 50px;
-    margin-right: 50px;
+    margin-left: 60px;
+    margin-right: 60px;
     background-color: white;
     padding: 40px;
 }
 .container_cards{
     display: flex;
     flex-wrap: wrap;
+    background-color: white;
 }
 .cards{
-    background-color: #d48f38;
     width: calc(100% /5);
+    margin-bottom: 25px;
+}
+.card img{
+    width: 100%;
+    display: block;
+}
+.card{
+    padding-left: 10px;
+    padding-right: 10px;;
+}
+.info_card{
+    text-align: center;  
+    background-color: #d48f38;
+    margin-left: 10px;
+    margin-right: 10px;   
 }
 </style>
