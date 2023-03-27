0c2b30b7ea51ee798a84f5cd8799155adfc63df8
Mikel3010 <mikelgremi30@gmail.com>
2023-03-27 17:56:53 +0200

    header

---
+++ vite-yu-gi-ho/src/App.vue
@@ -0,0 +1,18 @@
+<script >
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+  components:{
+    AppHeader,
+  }
+}
+
+</script>
+
+<template>
+<AppHeader></AppHeader>
+</template>
+
+<style lang="scss">
+@use './assets/scss/general.scss';
+</style>
---
+++ vite-yu-gi-ho/src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+export default{
+    components:{
+       name:'AppHeader',
+    }
+}
+</script>
+<template>
+<div class="container_header">
+    <div class="logo">
+        <img src="../../public/Yu-Gi-Oh!.svg" alt="">
+    </div>
+    <div class="titolo">
+        <h1>Yu-Gi-Ho Api</h1>
+    </div>
+</div>
+</template>
+<style lang="scss" scoped>
+.container_header{
+    height: 6.25rem;
+    display: flex;
+    align-items: center;
+}
+.logo{
+    height: 1.875rem;
+    width: 80px;
+    margin-left: 30px;
+    margin-right: 20px;
+}
+.logo img{
+    width: 100%;
+}
+
+</style>
